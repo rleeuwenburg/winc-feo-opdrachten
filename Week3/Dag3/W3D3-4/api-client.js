780417a655ff88fc4f045d260947221966fdea31
Raimon Leeuwenburg <r.leeuwenburg@gmail.com>
2020-02-20 22:25:40 +0100

    added excercises W3D3-4

--- Week3/Dag3/W3D3-4/api-client.js
+++ Week3/Dag3/W3D3-4/api-client.js
@@ -16,15 +16,64 @@ const API_KEY = inPrivateRepository;
 //     console.log(error);
 //   }
 // };
-
+// MovieGenres ophalen
 const getMovieGenres = async function() {
   try {
     const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`;
     const res = await fetch(genreUrl, { method: 'GET' });
-    const movieGenres = await res.json();
-    console.log('the genres in getMovieGenres function', movieGenres);
-    return movieGenres;
+    const result = await res.json();
+    console.log('the genres in getMovieGenres function', result.genres);
+    return result.genres;
   } catch (error) {
     console.log(error);
   }
 };
+
+// specifieke film ophalen obv ID
+const external_id = 'tt0068646';
+const external_source = 'imdb_id';
+
+const getMovie = async function() {
+  try {
+    const movieUrl = `https://api.themoviedb.org/3/find/${external_id}?api_key=${API_KEY}&external_source=${external_source}`;
+    const result = await fetch(movieUrl, { method: 'GET' });
+    const data = await result.json();
+    console.log('the data: ', data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getMovie();
+
+// toplijst films ophalen
+const getTopratedMovies = async function() {
+  try {
+    const topratedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+    const result = await fetch(topratedUrl, { method: 'GET' });
+    const data = await result.json();
+    console.log('the data: ', data);
+    return data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getTopratedMovies();
+
+// Films uit 1975
+
+const getMovies1975 = async function() {
+  try {
+    const url1975 = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&primary_release_year=1975&sort_by=popularity.desc&page=1`;
+    const result = await fetch(url1975, { method: 'GET' });
+    const data = await result.json();
+    console.log('the data: ', data);
+    return data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getMovies1975();
--- Week3/Dag3/W3D3-4/script.js
+++ Week3/Dag3/W3D3-4/script.js
@@ -11,30 +11,140 @@
 // Bedenk of deze functies synchroon óf asynchroon moeten zijn.
 // Ook in de functiebenaming zet je altijd de REST methode (GET PUT POST DELETE).
 // Bijvoorbeeld: Als ik een rating van een movie zou willen posten naar TheMovieDB dat zou ik daar de functie postMovieRating() van maken.
+// const createList = function() {
+//   const wrapper = document.createElement('div');
+//   document.body.appendChild(wrapper);
+//   const list = document.createElement('ul')
+//   // ul moet een ID krijgen
+// };
 
-const workWithMovieGenres = async function() {
+// // createList();
+
+// // Lijst even met de hand in html gezet
+
+// // Moviegenres in html lijst zetten
+const addMovieGenresToList = async function() {
+  const listMovieGenres = document.getElementById('listMovieGenres');
   const movieGenres = await getMovieGenres();
-  console.log('here is your data to do something:', movieGenres);
-};
 
-workWithMovieGenres();
-
-const createList = function() {
-  const wrapper = document.createElement('div', (id = 'wrapper'));
-  document.body.appendChild(wrapper);
-  const list = document.createElement('ul', (id = 'listMovieGenres'));
-  wrapper.appendChild(list);
-};
-
-createList();
-
-const addMovieGenresToList = function() {
   movieGenres.forEach(movieGenre => {
-    const listItemMovieGenre = document.createElement('li');
-    const listMovieGenres = document.getElementById('listMovieGenres');
-    listMovieGenres.appendChild(listItemMovieGenre);
-    listItemMovieGenre.innerHTML; // Genre en Id toevoegen
+    const li = document.createElement('li');
+    const liMovieGenre = document.createTextNode(
+      `genre: ${movieGenre.name}, id: ${movieGenre.id}`
+    );
+
+    li.appendChild(liMovieGenre);
+    listMovieGenres.appendChild(li);
   });
 };
 
 addMovieGenresToList();
+
+/* 
+W3D3-4: Deel 1, vraag2
+X Zoek film ID in documentatie van API
+/find/{external_id}
+external_id = imdb id
+Lion King = tt6105098
+
+X Gebruik Postman om get request te maken
+ x Juiste URL met endpoint (ID)
+ x HTTP functie GET
+ x API_KEY
+ x wat staat er nog meer in documentatie?
+X Doe dit nog 2x voor andere films
+X <h1> maken in html
+X filmtitel toevoegen
+  x Maak API request in JS met juiste film id 
+X Maak functie om titel onder h1 te plaatsen
+    x Selecteer h1 met getElementById
+    x maak een <li>, uiteindelijk <p> gedaan, want 1 film is geen lijst
+    x vul <li> met favoriete titel, hoe? zie hieronder
+    x hang <li> onder <h1>
+X hoe krijg ik mijn data?
+ array, object, array
+X hoe kan ik die gebruiken?
+movie.movie_results[0].title
+X innerHTML van element gebruiken om waarde (titel) in te voegen. Gedaan met textnode
+
+- door naar volgende vraag
+ */
+
+const addMovieToFavorite = async function() {
+  const headerFavorite = document.getElementById('my-favorite-movie');
+  const movie = await getMovie();
+  const favMovieTitle = movie.movie_results[0].title;
+  const pElement = document.createElement('p');
+  pElement.style.fontSize = '20px';
+  const pFavoriteMovie = document.createTextNode(
+    `My favorite movie is: ${favMovieTitle}`
+  );
+
+  pElement.appendChild(pFavoriteMovie);
+  headerFavorite.appendChild(pElement);
+};
+
+addMovieToFavorite();
+
+/*
+W3D3-4: Deel 1, vraag 3
+X Zoek toplijst in documentatie van API
+/movie/top_rated
+
+X h1 en ul maken in html met id="top-rated"
+X Maak API request in JS met juiste path in url
+X Maak functie om li's in lijst te plaatsen
+    x maak koppeling met API request mbv async/await
+    x Van top20 top 10 maken met slice
+    x Selecteer ul met getElementById
+    x maak een <li>
+    x vul <li> met titel mbv textnode
+    x hang <li> onder <ul>
+    x herhalen met forEach (dus moet zorgen dat ik werk in array)
+X hoe krijg ik mijn data?
+ array met objecten
+X hoe kan ik die gebruiken?
+met forEach
+
+- door naar volgende vraag, maar eerst eten
+ */
+
+const addTopTenMoviesToList = async function() {
+  const topratedMoviesData = await getTopratedMovies();
+  console.log(topratedMoviesData);
+  const filterEnglishMovies = topratedMoviesData.filter(
+    array => array.original_language === 'en'
+  );
+
+  const topratedList = document.getElementById('toprated-movies');
+  const makeTop10Movies = filterEnglishMovies.slice(0, 10);
+
+  makeTop10Movies.forEach(movie => {
+    const li = document.createElement('li');
+    const liToprated = document.createTextNode(movie.original_title);
+    li.appendChild(liToprated);
+    topratedList.appendChild(li);
+  });
+};
+
+addTopTenMoviesToList();
+
+const add1975MoviesToList = async function() {
+  const movies1975Data = await getMovies1975();
+  console.log(movies1975Data);
+  const filterEnglishMovies = movies1975Data.filter(
+    array => array.original_language === 'en'
+  );
+
+  const list1975 = document.getElementById('movies1975-list');
+  const makeTop10Movies1975 = filterEnglishMovies.slice(0, 10);
+
+  makeTop10Movies1975.forEach(movie => {
+    const li = document.createElement('li');
+    const liToprated = document.createTextNode(movie.original_title);
+    li.appendChild(liToprated);
+    list1975.appendChild(li);
+  });
+};
+
+add1975MoviesToList();
